ff35e8a4e6ae51eed8ef28281a86e4c7a0e1341e
michael fang <yingchih.fang@gmail.com>
2019-11-21 17:20:48 +0800

    20191121-2

---
+++ AZ203-GitHub/Allfiles/Labs/05/Starter/Console/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SimpleConsole
+{
+    
+    class Program
+    {
+
+            private const string _api = "https://smpapifij0727.azurewebsites.net/";
+            private static HttpClient _client = new HttpClient(new PollyHandler()){ BaseAddress = new Uri(_api) };
+
+
+        static void Main(string[] args)
+        {
+             Run().Wait();
+        }
+
+        static async Task Run()
+        { 
+            string response = await _client.GetStringAsync("/WeatherForecast");
+            Console.WriteLine(response);    
+         
+        }
+    }
+}
